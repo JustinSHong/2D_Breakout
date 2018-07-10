78357439594cc32f6827f2e750ae1ffc6b4916be
Justin Hong <jhong2009@gmail.com>
2018-07-09 20:30:44 -0700

    in index.js: added collisionDetection() handler that changes the ball's movement upon colliding with a brick

--- Vanilla_JS/js/index.js
+++ Vanilla_JS/js/index.js
@@ -77,6 +77,26 @@ function keyUpHandler(e) {
 	}
 }
 
+function collisionDetection() {
+	// compare position of bricks with the ball for every frame
+	for (var col = 0; col < brickColumnCount; col++) {
+		for (var row = 0; row < brickRowCount; row++) {
+			let brick = bricks[col][row];
+			// a collision with a brick occurs when the center of the ball is inside a brick's coordinates
+			// if a collision occurs, change the movement of the ball
+
+			if (
+				x > brick.x && // x position of the ball is greater than the x position of the brick
+				x < brick.x + brickWidth && // x position of the ball is less than the x position of the brick plus its width
+				y > brick.y && // y position of the ball is greater than the y position of the brick
+				y < brick.y + brickHeight // y position of the ball is less than the y position of the brick plus its height
+			) {
+				dy = -dy;
+			}
+		}
+	}
+}
+
 function drawBall() {
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
