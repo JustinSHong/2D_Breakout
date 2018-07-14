f0798951b0fb6765fea16acab1f5783bc07ee0ee
Justin Hong <jhong2009@gmail.com>
2018-07-14 13:31:11 -0700

    restructured project files, set up node server with web socket, created game.js main file

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io").listen(server);
+const helmet = require("helmet");
+const cors = require("cors");
+
+const PORT = 8081;
+
+// middleware
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
+// render the static files
+app.use(express.static(__dirname + "/public"));
+
+// Root - server serves index.html
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+	console.log("a user connected");
+	socket.on("disconnect", function() {
+		console.log("a user disconnected");
+	});
+});
+
+// start server at port 5000
+server.listen(PORT, () => {
+	console.log(`\n==API Running on port ${PORT} ==\n`);
+});
