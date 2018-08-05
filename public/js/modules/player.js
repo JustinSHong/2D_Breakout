d045687247fb2636c2dc69a632bb9aad93a445a2
JustinSHong <jhong2009@gmail.com>
2018-08-04 22:29:19 -0700

    player.js: adjusted size of game stats and their positions

--- public/js/modules/player.js
+++ public/js/modules/player.js
@@ -10,21 +10,21 @@ class Player {
 
 	// draw current score to the canvas
 	drawScore(canvas) {
-		canvas.ctx.font = "16px Arial";
+		canvas.ctx.font = "24px Arial";
 		canvas.ctx.fillStyle = "#0095DD";
 		canvas.ctx.fillText("Score: " + this.score, 8, 20);
-		canvas.ctx.fillText("Opponent's Score " + this.opponentScore, 8, 30);
+		canvas.ctx.fillText("Opponent's Score: " + this.opponentScore, 8, 40);
 	}
 
 	// draw current number of lives to the canvas
 	drawLives(canvas) {
-		canvas.ctx.font = "16px Arial";
+		canvas.ctx.font = "24px Arial";
 		canvas.ctx.fillStyle = "#0095DD";
-		canvas.ctx.fillText("Lives: " + this.lives, canvas.width - 65, 20);
+		canvas.ctx.fillText("Lives: " + this.lives, canvas.width - 90, 20);
 		canvas.ctx.fillText(
-			"Opponent's Lives " + this.opponentLives,
-			canvas.width - 145,
-			30
+			"Opponent's Lives: " + this.opponentLives,
+			canvas.width - 218,
+			40
 		);
 	}
 
