6df93a6560f157f1555ed09f9ef5001b571c4ab6
JustinSHong <jhong2009@gmail.com>
2018-08-04 21:58:54 -0700

    added opponentScore field to Player class; set up drawScore() to draw opponentScore

--- public/js/modules/player.js
+++ public/js/modules/player.js
@@ -1,9 +1,10 @@
 class Player {
 	constructor() {
-		// default number of player lives
 		this.lives = 3;
-		this.username = "";
 		this.score = 0;
+		this.opponentScore = 0;
+		this.x = 0;
+		this.y = 0;
 	}
 
 	// draw current score to the canvas
@@ -11,13 +12,14 @@ class Player {
 		canvas.ctx.font = "16px Arial";
 		canvas.ctx.fillStyle = "#0095DD";
 		canvas.ctx.fillText("Score: " + this.score, 8, 20);
+		canvas.ctx.fillText("Opponent's Score " + this.opponentScore, 8, 30);
 	}
 
 	// draw current number of lives to the canvas
 	drawLives(canvas) {
 		canvas.ctx.font = "16px Arial";
 		canvas.ctx.fillStyle = "#0095DD";
-		canvas.ctx.fillText("Lives: " + this.lives, canvas.width - 65, 20); // OBTAIN CANVAS WIDTH
+		canvas.ctx.fillText("Lives: " + this.lives, canvas.width - 65, 20);
 	}
 
 	// udpate player data
