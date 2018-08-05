74a15df54e5aae0f7a6aa4e55e2a6c64aaf0608e
JustinSHong <jhong2009@gmail.com>
2018-08-04 17:59:12 -0700

    server.js: create new player on connection event, share new player data with client

--- server.js
+++ server.js
@@ -22,12 +22,32 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
+const players = {};
+let playerID = 0;
+// client connects to server
 io.on("connection", function(socket) {
 	console.log("a user connected");
+	// emit to all connected clients
+	socket.emit("SocketId", addNewPlayer(socket.id));
+
+	// emit to all clients except the newest one
+	socket.broadcast.emit("SocketId", players[playerID]);
+
 	socket.on("disconnect", function() {
 		console.log("a user disconnected");
 	});
 });
+
+function addNewPlayer(id) {
+	playerID += 1;
+	players[playerID] = {
+		socketID: id,
+		playerID: playerID,
+		lives: 3,
+		score: 0
+	};
+	return players[playerID];
+}
 
 // start server at port 5000
 server.listen(PORT, () => {
