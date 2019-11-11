99da1b3e5c2d7cc3f233a91b231dff00ac9593d5
JustinSHong <jhong2009@gmail.com>
2019-11-10 21:34:06 -0800

    upgrade deps, prettier format

--- public/js/game.js
+++ public/js/game.js
@@ -1,117 +1,117 @@
 // modules
-import Canvas from "./modules/canvas";
-import Ball from "./modules/ball";
-import Brick from "./modules/brick";
-import Paddle from "./modules/paddle";
-import Player from "./modules/player";
-import socket from "./modules/client";
+import Canvas from './modules/canvas';
+import Ball from './modules/ball';
+import Brick from './modules/brick';
+import Paddle from './modules/paddle';
+import Player from './modules/player';
+import socket from './modules/client';
 
 // enable live reload
 document.write(
-    '<script src="https://' +
-        (location.host || "localhost").split(":")[0] +
+  '<script src="https://' +
+        (location.host || 'localhost').split(':')[0] +
         ':35729/livereload.js?snipver=1"></' +
-        "script>"
+        'script>'
 );
 
 class Game {
-    constructor() {
-        this.rightPressed = false;
-        this.leftPressed = false;
-        this.players = []; // holds all players in the game
+  constructor() {
+    this.rightPressed = false;
+    this.leftPressed = false;
+    this.players = []; // holds all players in the game
 
-        // listen for key press and key release
-        window.addEventListener("keydown", this.keyDownHandler, false);
-        window.addEventListener("keyup", this.keyUpHandler, false);
-        // listen for mouse movement
-        window.addEventListener("mousemove", this.mouseMoveHandler, false);
-        // listen for changes in game mode
-        easyMode.addEventListener("click", selectGameMode, false);
-        mediumMode.addEventListener("click", selectGameMode, false);
-        hardMode.addEventListener("click", selectGameMode, false);
-        veryHardMode.addEventListener("click", selectGameMode, false);
-    }
+    // listen for key press and key release
+    window.addEventListener('keydown', this.keyDownHandler, false);
+    window.addEventListener('keyup', this.keyUpHandler, false);
+    // listen for mouse movement
+    window.addEventListener('mousemove', this.mouseMoveHandler, false);
+    // listen for changes in game mode
+    easyMode.addEventListener('click', selectGameMode, false);
+    mediumMode.addEventListener('click', selectGameMode, false);
+    hardMode.addEventListener('click', selectGameMode, false);
+    veryHardMode.addEventListener('click', selectGameMode, false);
+  }
 
-    // initialize the game objects and the game loop
-    init() {
-        requestAnimationFrame(() => this.draw(canvas));
-    }
+  // initialize the game objects and the game loop
+  init() {
+    requestAnimationFrame(() => this.draw(canvas));
+  }
 
-    // main draw function of the game - initiates game loop
-    draw(canvas) {
-        // clear previous ball before drawing a new one
-        canvas.clear();
-        canvas.ctx.beginPath();
-        paddle.drawPaddle(canvas);
-        brick.drawBricks(canvas);
-        ball.drawBall(canvas);
-        player.drawScore(canvas);
-        player.drawLives(canvas);
-        canvas.detectBrickCollisions(ball, brick, player);
-        canvas.detectEdgeCollisions(ball, paddle, player);
+  // main draw function of the game - initiates game loop
+  draw(canvas) {
+    // clear previous ball before drawing a new one
+    canvas.clear();
+    canvas.ctx.beginPath();
+    paddle.drawPaddle(canvas);
+    brick.drawBricks(canvas);
+    ball.drawBall(canvas);
+    player.drawScore(canvas);
+    player.drawLives(canvas);
+    canvas.detectBrickCollisions(ball, brick, player);
+    canvas.detectEdgeCollisions(ball, paddle, player);
 
-        socket.emit("playerScored", player); // send player data to server
-        socket.emit("playerLife", player);
+    socket.emit('playerScored', player); // send player data to server
+    socket.emit('playerLife', player);
 
-        // move paddle right until the right edge of the canvas
-        if (
-            this.rightPressed &&
+    // move paddle right until the right edge of the canvas
+    if (
+      this.rightPressed &&
             paddle.paddleX < canvas.width - paddle.paddleWidth
-        ) {
-            paddle.update(7);
-        } else if (this.leftPressed && paddle.paddleX > 0) {
-            paddle.update(-7);
-        }
-        // update ball's position
-        ball.update();
-
-        if (pause === false) {
-            // animation loops
-            requestAnimationFrame(() => {
-                this.draw(canvas);
-            });
-        }
+    ) {
+      paddle.update(7);
+    } else if (this.leftPressed && paddle.paddleX > 0) {
+      paddle.update(-7);
     }
+    // update ball's position
+    ball.update();
 
-    // check if a key was pressed
-    keyDownHandler(e) {
-        if (e.keyCode === 39) {
-            // right cursor key pressed
-            g.rightPressed = true;
-        } else if (e.keyCode === 37) {
-            // left cursor key pressed
-            g.leftPressed = true;
-        } else if (e.keyCode === 80) {
-            // pause key pressed
-            pause = !pause;
-            if (pause === false) {
-                g.init();
-            }
-        } else if (e.keyCode === 81) {
-            alert("You quit. Game Over!");
-            document.location.reload();
-        }
+    if (pause === false) {
+      // animation loops
+      requestAnimationFrame(() => {
+        this.draw(canvas);
+      });
     }
+  }
 
-    // check if a key was released
-    keyUpHandler(e) {
-        // reset key state to default
-        if (e.keyCode === 39) {
-            // right cursor key released
-            g.rightPressed = false;
-        } else if (e.keyCode === 37) {
-            // right cursor key released
-            g.leftPressed = false;
-        }
+  // check if a key was pressed
+  keyDownHandler(e) {
+    if (e.keyCode === 39) {
+      // right cursor key pressed
+      g.rightPressed = true;
+    } else if (e.keyCode === 37) {
+      // left cursor key pressed
+      g.leftPressed = true;
+    } else if (e.keyCode === 80) {
+      // pause key pressed
+      pause = !pause;
+      if (pause === false) {
+        g.init();
+      }
+    } else if (e.keyCode === 81) {
+      alert('You quit. Game Over!');
+      document.location.reload();
     }
+  }
 
-    // move paddle relative to the mouse position within canvas
-    mouseMoveHandler(e) {
-        let relativeX = e.clientX - canvas.canvas.offsetLeft;
-        if (relativeX > 0 && relativeX < canvas.width) {
-            paddle.paddleX = relativeX - paddle.paddleWidth / 2;
-        }
+  // check if a key was released
+  keyUpHandler(e) {
+    // reset key state to default
+    if (e.keyCode === 39) {
+      // right cursor key released
+      g.rightPressed = false;
+    } else if (e.keyCode === 37) {
+      // right cursor key released
+      g.leftPressed = false;
     }
+  }
+
+  // move paddle relative to the mouse position within canvas
+  mouseMoveHandler(e) {
+    let relativeX = e.clientX - canvas.canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+      paddle.paddleX = relativeX - paddle.paddleWidth / 2;
+    }
+  }
 }
 
 // pause flag
@@ -120,61 +120,61 @@ let pause = true;
 // < ===== STARTING THE GAME ===== >
 
 let mode = {
-    name: "very easy",
-    dx: 1.5,
-    dy: -1.5,
-    lives: 5
+  name: 'very easy',
+  dx: 1.5,
+  dy: -1.5,
+  lives: 5
 };
 
 // game mode buttons
-const easyMode = document.querySelector(".easy-mode-btn");
-const mediumMode = document.querySelector(".medium-mode-btn");
-const hardMode = document.querySelector(".hard-mode-btn");
-const veryHardMode = document.querySelector(".veryHard-mode-btn");
+const easyMode = document.querySelector('.easy-mode-btn');
+const mediumMode = document.querySelector('.medium-mode-btn');
+const hardMode = document.querySelector('.hard-mode-btn');
+const veryHardMode = document.querySelector('.veryHard-mode-btn');
 
 function selectGameMode(e) {
-    const { name } = e.target;
-    if (name === "easy") {
-        mode = {
-            name: "easy",
-            dx: 4,
-            dy: -4,
-            lives: 3
-        };
-        ball = new Ball(canvas.height, canvas.width, mode);
-        player = new Player(mode);
-        brick = new Brick(canvas);
-    } else if (name === "medium") {
-        mode = {
-            name: "medium",
-            dx: 6,
-            dy: -6,
-            lives: 3
-        };
-        ball = new Ball(canvas.height, canvas.width, mode);
-        player = new Player(mode);
-        brick = new Brick(canvas);
-    } else if (name === "hard") {
-        mode = {
-            name: "hard",
-            dx: 8,
-            dy: -8,
-            lives: 2
-        };
-        ball = new Ball(canvas.height, canvas.width, mode);
-        player = new Player(mode);
-        brick = new Brick(canvas);
-    } else {
-        mode = {
-            name: "veryHard",
-            dx: 10,
-            dy: -10,
-            lives: 2
-        };
-        ball = new Ball(canvas.height, canvas.width, mode);
-        player = new Player(mode);
-        brick = new Brick(canvas);
-    }
+  const { name } = e.target;
+  if (name === 'easy') {
+    mode = {
+      name: 'easy',
+      dx: 4,
+      dy: -4,
+      lives: 3
+    };
+    ball = new Ball(canvas.height, canvas.width, mode);
+    player = new Player(mode);
+    brick = new Brick(canvas);
+  } else if (name === 'medium') {
+    mode = {
+      name: 'medium',
+      dx: 6,
+      dy: -6,
+      lives: 3
+    };
+    ball = new Ball(canvas.height, canvas.width, mode);
+    player = new Player(mode);
+    brick = new Brick(canvas);
+  } else if (name === 'hard') {
+    mode = {
+      name: 'hard',
+      dx: 8,
+      dy: -8,
+      lives: 2
+    };
+    ball = new Ball(canvas.height, canvas.width, mode);
+    player = new Player(mode);
+    brick = new Brick(canvas);
+  } else {
+    mode = {
+      name: 'veryHard',
+      dx: 10,
+      dy: -10,
+      lives: 2
+    };
+    ball = new Ball(canvas.height, canvas.width, mode);
+    player = new Player(mode);
+    brick = new Brick(canvas);
+  }
 }
 
 const canvas = new Canvas();
@@ -185,13 +185,13 @@ let player = new Player(mode);
 const g = new Game(); // instantiate a game
 
 // store your opponent's score
-socket.on("otherPlayerScore", function(data) {
-    player.opponentScore = data;
+socket.on('otherPlayerScore', function(data) {
+  player.opponentScore = data;
 });
 
 // store your opponent's life count
-socket.on("otherPlayerLife", function(data) {
-    player.opponentLives = data;
+socket.on('otherPlayerLife', function(data) {
+  player.opponentLives = data;
 });
 
 g.init(); // start the game loop
--- public/js/modules/brick.js
+++ public/js/modules/brick.js
@@ -1,47 +1,54 @@
 class Brick {
-	constructor() {
-		// brick properties
-		this.brickColor = "#0095DD";
-		this.brickRowCount = 10;
-		this.brickColumnCount = 7;
-		this.brickWidth = 75;
-		this.brickHeight = 20;
-		this.brickPadding = 10;
-		this.brickOffsetTop = 50;
-		this.brickOffsetLeft = 160;
-		// initialize bricks
-		this.bricks = [];
+  constructor() {
+    // brick properties
+    this.brickColor = '#0095DD';
+    this.brickRowCount = 10;
+    this.brickColumnCount = 7;
+    this.brickWidth = 75;
+    this.brickHeight = 20;
+    this.brickPadding = 10;
+    this.brickOffsetTop = 50;
+    this.brickOffsetLeft = 160;
+    // initialize bricks
+    this.bricks = [];
 
-		for (let col = 0; col < this.brickColumnCount; col++) {
-			this.bricks[col] = [];
-			for (let row = 0; row < this.brickRowCount; row++) {
-				this.bricks[col][row] = { x: 0, y: 0, status: 1 }; // default brick properties
-			}
-		}
-	}
+    for (let col = 0; col < this.brickColumnCount; col++) {
+      this.bricks[col] = [];
+      for (let row = 0; row < this.brickRowCount; row++) {
+        this.bricks[col][row] = { x: 0, y: 0, status: 1 }; // default brick properties
+      }
+    }
+  }
 
-	// draw brick grid to the canvas
-	drawBricks(canvas) {
-		for (let col = 0; col < this.brickColumnCount; col++) {
-			for (let row = 0; row < this.brickRowCount; row++) {
-				let brick = this.bricks[col][row];
-				if (brick.status === 1) {
-					// ball did not collide with brick
-					let brickX =
-						col * (this.brickWidth + this.brickPadding) + this.brickOffsetLeft; // offset X position from previous brick
-					let brickY =
-						row * (this.brickHeight + this.brickPadding) + this.brickOffsetTop; // offset Y position from previous brick
-					brick.x = brickX;
-					brick.y = brickY;
-					canvas.ctx.beginPath();
-					canvas.ctx.rect(brick.x, brick.y, this.brickWidth, this.brickHeight);
-					canvas.ctx.fillStyle = this.brickColor;
-					canvas.ctx.fill();
-					canvas.ctx.closePath();
-				}
-			}
-		}
-	}
+  // draw brick grid to the canvas
+  drawBricks(canvas) {
+    for (let col = 0; col < this.brickColumnCount; col++) {
+      for (let row = 0; row < this.brickRowCount; row++) {
+        let brick = this.bricks[col][row];
+        if (brick.status === 1) {
+          // ball did not collide with brick
+          let brickX =
+                        col * (this.brickWidth + this.brickPadding) +
+                        this.brickOffsetLeft; // offset X position from previous brick
+          let brickY =
+                        row * (this.brickHeight + this.brickPadding) +
+                        this.brickOffsetTop; // offset Y position from previous brick
+          brick.x = brickX;
+          brick.y = brickY;
+          canvas.ctx.beginPath();
+          canvas.ctx.rect(
+            brick.x,
+            brick.y,
+            this.brickWidth,
+            this.brickHeight
+          );
+          canvas.ctx.fillStyle = this.brickColor;
+          canvas.ctx.fill();
+          canvas.ctx.closePath();
+        }
+      }
+    }
+  }
 }
 
 export default Brick;
