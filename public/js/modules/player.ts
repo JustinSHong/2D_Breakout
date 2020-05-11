3082e7edce98457e866bfed760d68fa9222765b1
JustinSHong <jhong2009@gmail.com>
2020-05-10 20:29:27 -0700

    remove fields involving opponent data

--- public/js/modules/player.ts
+++ public/js/modules/player.ts
@@ -3,8 +3,6 @@ import { ICanvas } from './canvas'
 export interface IPlayer {
 	lives: number
 	score: number
-	opponentScore: number
-	opponentLives: number
 	mode: any
 	drawScore(canvas: ICanvas): void
 	drawLives(canvas: ICanvas): void
@@ -13,14 +11,10 @@ export interface IPlayer {
 class Player implements IPlayer {
 	public lives: number
 	public score: number
-	public opponentScore: number
-	public opponentLives: number
 
 	constructor(public mode: any) {
 		this.lives = mode.lives
 		this.score = 0
-		this.opponentScore = 0
-		this.opponentLives = 0
 	}
 
 	// draw current score to the canvas
@@ -28,7 +22,6 @@ class Player implements IPlayer {
 		canvas.ctx.font = '24px Arial'
 		canvas.ctx.fillStyle = '#0095DD'
 		canvas.ctx.fillText('Score: ' + this.score, 8, 20)
-		canvas.ctx.fillText("Opponent's Score: " + this.opponentScore, 8, 40)
 	}
 
 	// draw current number of lives to the canvas
@@ -36,11 +29,6 @@ class Player implements IPlayer {
 		canvas.ctx.font = '24px Arial'
 		canvas.ctx.fillStyle = '#0095DD'
 		canvas.ctx.fillText('Lives: ' + this.lives, canvas.width - 90, 20)
-		canvas.ctx.fillText(
-			"Opponent's Lives: " + this.opponentLives,
-			canvas.width - 218,
-			40
-		)
 	}
 
 	// udpate player data
