97ea828507082e1b57bc22a06729319307c780b3
Dripcoding <dripcoding@gmail.com>
2020-08-08 11:51:20 -0700

    set up dom testing library deps and test suite

---
+++ __tests__/app.test.ts
@@ -0,0 +1,10 @@
+import { getByRole, getByText, getByTestId } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('application', () => {
+	test('it should have a navigation bar', () => {})
+
+	test('it should have a tool bar for game controls', () => {})
+
+	test('it should have a canvas', () => {})
+})
