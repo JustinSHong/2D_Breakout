8274b52ddeff2c6a1ef8b222e9187cfd0fb1808d
unknown <jhong2009@gmail.com>
2020-08-08 12:34:40 -0700

    add navigation bar tests

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,8 +1,52 @@
-import { getByRole, getByText, getByTestId } from '@testing-library/dom'
-import '@testing-library/jest-dom/extend-expect'
+const fs = require('fs')
+const path = require('path')
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8')
 
 describe('application', () => {
-	test('it should have a navigation bar', () => {})
+	beforeEach(() => {
+		document.documentElement.innerHTML = html.toString()
+	})
+
+	afterEach(() => {
+		jest.resetModules()
+	})
+
+	test('it should have a navigation bar', () => {
+		const navBar = document.querySelector('.navbar')
+		const navBarBrand = document.querySelector('.navbar-brand')
+		const navBarToggler = document.querySelector('.navbar-toggler')
+		const navBarItem = document.querySelectorAll('.nav-item')
+
+		// nav bar brand
+		expect(navBar).toBeTruthy()
+		expect(navBarBrand).toBeTruthy()
+		expect(navBarBrand instanceof HTMLAnchorElement).toBe(true)
+		expect(navBarBrand?.textContent).toBe('2D Breakout')
+		expect(navBarToggler).toBeTruthy()
+		// nav bar items
+		expect(navBarItem).toBeTruthy()
+		expect(navBarItem.length).toBe(4)
+		expect(navBarItem[0] instanceof HTMLAnchorElement).toBe(true)
+		expect(navBarItem[0].textContent).toBe('About')
+		expect(navBarItem[0].getAttribute('data-toggle')).toBe('modal')
+		expect(navBarItem[0].getAttribute('data-target')).toBe('#aboutModal')
+
+		expect(navBarItem[1] instanceof HTMLAnchorElement).toBe(true)
+		expect(navBarItem[1].textContent).toBe('Controls')
+		expect(navBarItem[1].getAttribute('data-toggle')).toBe('modal')
+		expect(navBarItem[1].getAttribute('data-target')).toBe('#controlsModal')
+
+		expect(navBarItem[2] instanceof HTMLAnchorElement).toBe(true)
+		expect(navBarItem[2].textContent).toBe('Github Repo')
+		expect(navBarItem[2].getAttribute('href')).toBe(
+			'https://github.com/JustinSHong/2D_Breakout'
+		)
+
+		expect(navBarItem[3] instanceof HTMLAnchorElement).toBe(true)
+		expect(navBarItem[3].textContent).toBe('Settings')
+		expect(navBarItem[3].getAttribute('data-toggle')).toBe('modal')
+		expect(navBarItem[3].getAttribute('data-target')).toBe('#settingsModal')
+	})
 
 	test('it should have a tool bar for game controls', () => {})
 
