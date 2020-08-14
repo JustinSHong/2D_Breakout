ad833d26ea5864e9ed0f237cd18b2ca5289d7acc
unknown <jhong2009@gmail.com>
2020-08-13 22:27:48 -0700

    configure healthcheck, n runs

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,13 @@
 module.exports = {
 	ci: {
+		healthcheck: {
+			fatal: true,
+			checks: ['githubToken'],
+		},
 		collect: {
 			url: ['http://localhost:8081/'],
 			startServerCommand: 'node server.js',
+			numberOfRuns: 5,
 		},
 		upload: {
 			target: 'temporary-public-storage',
