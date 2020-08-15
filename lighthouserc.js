5df91ad62a400728c703ac3c9c8fa5cc33000647
unknown <jhong2009@gmail.com>
2020-08-15 14:45:30 -0700

    add lighthouse category score thresholds

--- lighthouserc.js
+++ lighthouserc.js
@@ -14,5 +14,13 @@ module.exports = {
 		upload: {
 			target: 'temporary-public-storage',
 		},
+		assert: {
+			assertions: {
+				'categories:performance': ['error', { minScore: 0.9 }],
+				'categories:accessibility': ['error', { minScore: 0.9 }],
+				'categories:best-practices': ['error', { minScore: 0.9 }],
+				'categories:seo': ['error', { minScore: 0.9 }],
+			},
+		},
 	},
 }
