201253d8a8841d308aea92d66b2738f4f9098085
Dripcoding <dripcoding@gmail.com>
2020-08-15 17:51:37 -0700

    test

--- lighthouse-mobile.js
+++ lighthouse-mobile.js
@@ -12,8 +12,8 @@ module.exports = {
 		},
 		upload: {
 			target: 'lhci',
-			serverBaseUrl: $LHCI_URL,
-			token: $LHCI_TOKEN,
+			serverBaseUrl: '$LHCI_URL',
+			token: '$LHCI_TOKEN',
 		},
 		assert: {
 			assertions: {
